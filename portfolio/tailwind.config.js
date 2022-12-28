e8bbade39f8eb0d76a1be846cf1f8a59d86c8916
Jordan Rose <jrose8775@gmail.com>
2022-12-28 11:24:02 -0400

    initial personal portfolio

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      }
+    },
+  },
+  plugins: [],
+}
